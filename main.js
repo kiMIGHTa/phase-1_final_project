16daa1f3729f85c36f76c7f20f8fac893a6f8c30
Dennis Kimaita <dkimaita22@gmail.com>
2023-07-08 10:38:28 +0300

    render recipes

--- main.js
+++ main.js
@@ -40,4 +40,46 @@ function fetchData() {
       getObeseRecipes(data)
     }
   }
+
+  function getNormalWeightRecipes(mealPlans) {
+    if (mealPlans[0].category === 'Normal Weight') {
+      let result = document.getElementById('result')
+      let recipes = mealPlans[0].recipes
+  
+      recipes.forEach((recipe) => {
+        let output = document.createElement('div')
+        output.className = "output"
+  
+        let recipeName = document.createElement('h3')
+        recipeName.innerText = recipe.name
+        output.appendChild(recipeName)
+  
+        let ingredientsTitle = document.createElement('h4')
+        ingredientsTitle.innerText = 'Ingredients'
+        output.appendChild(ingredientsTitle)
+  
+        let ingredientsList = document.createElement('ul')
+        recipe.ingredients.forEach((ingredient) => {
+          let ingredientItem = document.createElement('li')
+          ingredientItem.innerText = ingredient
+          ingredientsList.appendChild(ingredientItem)
+        })
+        output.appendChild(ingredientsList)
+  
+        let instructionsTitle = document.createElement('h4')
+        instructionsTitle.innerText = 'Instructions'
+        output.appendChild(instructionsTitle)
+  
+        let instructionsList = document.createElement('ol')
+        recipe.instructions.forEach((instruction) => {
+          let instructionItem = document.createElement('li')
+          instructionItem.innerText = instruction
+          instructionsList.appendChild(instructionItem)
+        })
+        output.appendChild(instructionsList)
+  
+        result.appendChild(output)
+      })
+    }
+ }
   
